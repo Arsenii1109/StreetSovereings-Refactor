d38a215af7521db0c8245e5051923673a7f76905
kybex11 <artm.22@internet.ru>
2024-08-31 18:53:19 +0000

    update gameobject

--- StreetSovereings_.Game/src/objects/GameObject.cs
+++ StreetSovereings_.Game/src/objects/GameObject.cs
@@ -1,11 +1,41 @@
-namespace StreetSovereings_.objects;
-
-public class GameObject
+// GameObject.cs
+namespace StreetSovereings_.objects
 {
-    
-}
+    public abstract class GameObject
+    {
+        public string Id { get; set; }
+    }
 
-public class ObjectManager
-{
-    private Dictionary<string, List<GameObject>> _objects = new ();
+    public interface IGameObjectRepository<T> where T : GameObject
+    {
+        void Add(T obj);
+        T Get(string id);
+        IEnumerable<T> GetAll();
+        void Remove(string id);
+    }
+
+    public class GameObjectRepository<T> : IGameObjectRepository<T> where T : GameObject
+    {
+        private readonly Dictionary<string, T> _objects = new Dictionary<string, T>();
+
+        public void Add(T obj)
+        {
+            _objects[obj.Id] = obj;
+        }
+
+        public T Get(string id)
+        {
+            return _objects.TryGetValue(id, out T obj) ? obj : null;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _objects.Values;
+        }
+
+        public void Remove(string id)
+        {
+            _objects.Remove(id);
+        }
+    }
 }
