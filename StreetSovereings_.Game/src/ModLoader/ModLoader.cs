849003f74c79bf84dda804f88eff78c8d2bb3b67
kybex11 <artm.22@internet.ru>
2024-08-31 20:24:38 +0000

    update modloader

--- StreetSovereings_.Game/src/ModLoader/ModLoader.cs
+++ StreetSovereings_.Game/src/ModLoader/ModLoader.cs
@@ -11,7 +11,7 @@ namespace StreetSovereings_.src.ModLoader
 
         public static void Initialize(Renderer.Game game)
         {
-            _gameInstance = game; 
+            _gameInstance = game;
         }
 
         internal static void Load()
@@ -30,6 +30,7 @@ namespace StreetSovereings_.src.ModLoader
                     ProcessFile(file);
                 }
             }
+            else
             {
                 Console.WriteLine($"Directory '{modsDirectory}' does not exist.\nSkipping Error.");
             }
@@ -41,6 +42,7 @@ namespace StreetSovereings_.src.ModLoader
 
             string[] printStatements = fileContent.Split(new[] { "print(" }, StringSplitOptions.None);
             string[] addPlaneStatements = fileContent.Split(new[] { "addPlane(" }, StringSplitOptions.None);
+            string[] addCubeStatements = fileContent.Split(new[] { "addCube(" }, StringSplitOptions.None);
 
             foreach (string statement in printStatements.Skip(1))
             {
@@ -59,6 +61,16 @@ namespace StreetSovereings_.src.ModLoader
                 {
                     string addPlaneArgument = statement.Substring(0, endIndex).Trim();
                     addSublity(addPlaneArgument);
+                }
+            }
+
+            foreach (string statement in addCubeStatements.Skip(1))
+            {
+                int endIndex = statement.IndexOf(')');
+                if (endIndex != -1)
+                {
+                    string addCubeArgument = statement.Substring(0, endIndex).Trim();
+                    addCube(addCubeArgument);
                 }
             }
         }
@@ -81,11 +93,36 @@ namespace StreetSovereings_.src.ModLoader
                     float.Parse(parameters[8])
                 );
 
-                _gameInstance.AddPlane(x, y, z, sizeX, sizeZ, rgba); // Используйте существующий экземпляр
+                _gameInstance.AddPlane(x, y, z, sizeX, sizeZ, rgba);
             }
             else
             {
                 Console.WriteLine("Invalid argument format for AddPlane.");
+            }
+        }
+
+        static void addCube(string argument)
+        {
+            Console.WriteLine($"Adding cube with argument: {argument}");
+            var parameters = argument.Split(',');
+            if (parameters.Length == 8)
+            {
+                float x = float.Parse(parameters[0]);
+                float y = float.Parse(parameters[1]);
+                float z = float.Parse(parameters[2]);
+                Vector4 rgba = new Vector4(
+                    float.Parse(parameters[3]),
+                    float.Parse(parameters[4]),
+                    float.Parse(parameters[5]),
+                    float.Parse(parameters[6])
+                );
+                float mass = float.Parse(parameters[7]);
+
+                _gameInstance.AddCube(x, y, z, rgba, mass);
+            }
+            else
+            {
+                Console.WriteLine("Invalid argument format for AddCube.");
             }
         }
 
