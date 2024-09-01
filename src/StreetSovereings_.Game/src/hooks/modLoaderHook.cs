127f1f677268df6221129ce3c3efe7741861f38e
Akemiko <67152382+Arsenii1109@users.noreply.github.com>
2024-09-01 13:03:29 +0800

    Refactor directory

---
+++ src/StreetSovereings_.Game/src/hooks/modLoaderHook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace StreetSovereigns.src.hooks
+{
+    internal class ModLoaderHook
+    {
+        internal static bool CheckModLoader()
+        {
+            return File.Exists("./SSModLoader.exe");
+        }
+    }
+}
